6c011228bd747043c6e3d1dc6a24e8f973cc4b03
Joel Kong <joelkong2001@gmail.com>
2024-02-12 13:59:46 +0800

    sdf

--- desktop/src/com/mygdx/game/Controls/ControlManager.java
+++ desktop/src/com/mygdx/game/Controls/ControlManager.java
@@ -1,13 +1,24 @@
 package com.mygdx.game.Controls;
 
+import com.mygdx.game.Scenes.GameScene;
+import com.mygdx.game.Scenes.StartScene;
+
 // Control Manager Class
 public class ControlManager {
     // Declare Variables
     private PlayerControls playerControls;
+    private startSceneControls startSceenControls;
 
     // Default Constructor to initialise children
     public ControlManager() {
         this.playerControls = new PlayerControls();
+        this.startSceenControls = new StartScreenControls();
+    }
+
+    public void mapControls(GameScene scene) {
+        if (scene instanceof StartScene) {
+            setCurrentControls(startSceenControls);
+        }
     }
 
     // Get Player Controls
--- desktop/src/com/mygdx/game/Simulation/SimulationManager.java
+++ desktop/src/com/mygdx/game/Simulation/SimulationManager.java
@@ -1,4 +1,50 @@
 package com.mygdx.game.Simulation;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.AI.AIControlManager;
+import com.mygdx.game.Collisions.CollisionManager;
+import com.mygdx.game.Controls.ControlManager;
+import com.mygdx.game.Entity.EntityManager;
+import com.mygdx.game.InputOutput.InputOutputManager;
+import com.mygdx.game.Scenes.GameScene;
+import com.mygdx.game.Scenes.SceneManager;
+import com.mygdx.game.Scenes.StartScene;
+
 public class SimulationManager {
+    private SimulationManager simulationManager;
+    private EntityManager entityManager;
+    private SceneManager sceneManager;
+    private InputOutputManager inputOutputManager;
+    private ControlManager controlManager;
+    private AIControlManager aiControlManager;
+    private CollisionManager collisionManager;
+    private SpriteBatch batch;
+
+    batch = new SpriteBatch();
+    entityManager = new EntityManager();
+    sceneManager = new SceneManager();
+    inputOutputManager = new InputOutputManager();
+    controlManager = new ControlManager();
+    aiControlManager = new AIControlManager();
+    collisionManager = new CollisionManager();
+
+
+    public void initialiseSimulation() {
+        sceneManager.loadScreen(sceneManager.getStartScreen);
+    }
+
+    public void updateSimulation() {
+        currentScene = sceneManager.getCurrentScene;
+        sceneManager.loadScreen(currentScene);
+        if (currentScene instanceof StartScene) {
+            controlManager.mapControls(currentScene);
+        } else if (currentScene instanceof GameScene) {
+            initialisePlayerMovement();
+        }
+        // collision
+    }
+
+    public void endSimulation() {
+        //dispose
+    }
 }
