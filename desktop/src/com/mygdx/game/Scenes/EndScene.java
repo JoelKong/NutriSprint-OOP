efd6ce8d8209e8840a0969d21ccf8e71297c1663
Dinie Zikry <77114684+diniezikryy@users.noreply.github.com>
2024-02-16 14:28:20 +0800

    added text on start and end scenes

--- desktop/src/com/mygdx/game/Scenes/EndScene.java
+++ desktop/src/com/mygdx/game/Scenes/EndScene.java
@@ -3,10 +3,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
 public class EndScene extends Scenes implements Screen {
+    private Stage stage;
+    private BitmapFont font;
+    private SpriteBatch batch;
+    private GlyphLayout layout;
 
     public EndScene() {
         super(3, "endScene");
+        font = new BitmapFont();
+        batch = new SpriteBatch();
+        layout = new GlyphLayout();
     }
 
     @Override
@@ -18,6 +31,18 @@ public class EndScene extends Scenes implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1); // setting clear color to green
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // clear screen
+
+        layout.setText(font, "Game Ended");
+
+        float textWidth = layout.width;
+        float textHeight = layout.height;
+
+        float x = (Gdx.graphics.getWidth() - textWidth) / 2;
+        float y = (Gdx.graphics.getHeight() - textHeight) / 2;
+
+        batch.begin();
+            font.draw(batch, layout, x, y);
+        batch.end();
 
         // Call render from Scene if needed and any additional render logic
     }
@@ -46,6 +71,7 @@ public class EndScene extends Scenes implements Screen {
 
     @Override
     public void dispose() {
-        // Any dispose logic
+        font.dispose();
+        batch.dispose();
     }
 }
--- desktop/src/com/mygdx/game/Scenes/StartScene.java
+++ desktop/src/com/mygdx/game/Scenes/StartScene.java
@@ -3,11 +3,24 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
 // Start Scene class inherited from Scenes
 public class StartScene extends Scenes implements Screen {
+    private Stage stage;
+    private BitmapFont font;
+    private SpriteBatch batch;
+    private GlyphLayout layout;
 
     public StartScene() {
         super(1, "startScene");
+        font = new BitmapFont();
+        batch = new SpriteBatch();
+        layout = new GlyphLayout();
     }
 
     @Override
@@ -20,6 +33,18 @@ public class StartScene extends Scenes implements Screen {
         Gdx.gl.glClearColor(0, 1, 0, 1); // setting clear color to green
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // clear screen
         // Call render from Scene if needed and any additional render logic
+
+        layout.setText(font, "Press 'Enter' to start game.");
+
+        float textWidth = layout.width;
+        float textHeight = layout.height;
+
+        float x = (Gdx.graphics.getWidth() - textWidth) / 2;
+        float y = (Gdx.graphics.getHeight() - textHeight) / 2;
+
+        batch.begin();
+            font.draw(batch, layout, x, y);
+        batch.end();
     }
 
     // Implement or override other methods from Scene if needed
@@ -48,5 +73,7 @@ public class StartScene extends Scenes implements Screen {
     @Override
     public void dispose() {
         // Any dispose logic
+        font.dispose();
+        batch.dispose();
     }
 }
